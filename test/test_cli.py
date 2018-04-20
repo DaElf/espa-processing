f94cc4e7c850632c0516fd868961953ab078fcc4
Jake Brinkmann <jakebrinkmann@gmail.com>
2018-04-20 18:56:44 -0500

    TST: get tests working again

--- test/test_cli.py
+++ test/test_cli.py
@@ -1,309 +1,240 @@
 #!/usr/bin/env python
 
-
-import os
-import unittest
-from argparse import ArgumentParser
-
+import pytest
 
 from processing import cli
 
 
-class TestCLI(unittest.TestCase):
+@pytest.fixture(scope="module")
+def parser():
+    return cli.build_command_line_parser()
+
+@pytest.fixture(scope='function')
+def options():
+    return ['--order-id', 'ORDERID',
+                      '--input-product-id', 'PRODUCT_ID',
+                      '--input-url', 'file://',
+                      '--espa-api', 'skip_api',
+                      '--work-dir', '.',
+                      '--dist-method', 'local',
+                      '--dist-dir', '.']
+
+
+class TestCLI(object):
     """Test the cli.py methods"""
 
-    def setUp(self):
-        self.parser = cli.build_command_line_parser()
-        self.static_options = ['--order-id', 'ORDERID',
-                               '--input-product-id', 'PRODUCT_ID',
-                               '--input-url', 'file://',
-                               '--espa-api', 'skip_api',
-                               '--work-dir', '.',
-                               '--dist-method', 'local',
-                               '--dist-dir', '.']
-
-    def tearDown(self):
-        pass
-
     def test_load_template(self):
-        cli.load_template('order_template.json')
-        with self.assertRaises(cli.BadTemplateError) as context:
-            cli.load_template('unittests/empty.json')
-        with self.assertRaises(ValueError) as context:
-            cli.load_template('unittests/garbage.json')
+        import os
+        print(os.getcwd())
+        print('$'*25)
+        cli.load_template('processing/order_template.json')
+        with pytest.raises(cli.BadTemplateError):
+            cli.load_template('test/test-orders/empty.json')
+        with pytest.raises(ValueError):
+            cli.load_template('test/test-orders/garbage.json')
 
-    def test_extents_not_specified(self):
-        options = [x for x in self.static_options]
+    def test_extents_not_specified(self, parser, options):
         options.extend(['--product-type', 'landsat'])
         options.extend(['--output-format', 'envi'])
 
-        args = self.parser.parse_args(options)
-        self.assertFalse(cli.check_for_extents(args))
+        args = parser.parse_args(options)
+        assert not cli.check_for_extents(args)
 
-    def test_extents_specified(self):
-        # Check missing options
-        options = [x for x in self.static_options]
+    def test_extents_missing_minx(self, parser, options):
         options.extend(['--product-type', 'landsat'])
         options.extend(['--output-format', 'envi'])
-
-        # Missing minx
         options.extend(['--extent-maxx', '2.0'])
-        try:
-            args = self.parser.parse_args(options)
+        with pytest.raises(cli.MissingExtentError):
+            args = parser.parse_args(options)
             cli.check_for_extents(args)
-        except cli.MissingExtentError:
-            pass
 
-        # Missing maxx
-        options = [x for x in self.static_options]
+    def test_extents_missing_specified(self, parser, options):
         options.extend(['--product-type', 'landsat'])
         options.extend(['--output-format', 'envi'])
         options.extend(['--extent-minx', '1.0'])
-        try:
-            args = self.parser.parse_args(options)
+
+        # Missing maxx
+        with pytest.raises(cli.MissingExtentError):
+            args = parser.parse_args(options)
             cli.check_for_extents(args)
-        except cli.MissingExtentError:
-            pass
 
         # Missing miny
         options.extend(['--extent-maxx', '2.0'])
-        try:
-            args = self.parser.parse_args(options)
+        with pytest.raises(cli.MissingExtentError):
+            args = parser.parse_args(options)
             cli.check_for_extents(args)
-        except cli.MissingExtentError:
-            pass
 
         # Missing maxy
         options.extend(['--extent-miny', '1.0'])
-        try:
-            args = self.parser.parse_args(options)
+        with pytest.raises(cli.MissingExtentError):
+            args = parser.parse_args(options)
             cli.check_for_extents(args)
-        except cli.MissingExtentError:
-            pass
 
         # Check all options
         options.extend(['--extent-maxy', '2.0'])
 
-        args = self.parser.parse_args(options)
-        self.assertTrue(cli.check_for_extents(args))
+        args = parser.parse_args(options)
+        assert cli.check_for_extents(args)
 
-    def test_sinu_specified(self):
+    def test_not_sinu_specified(self, parser, options):
         # Check not sinu
-        options = [x for x in self.static_options]
         options.extend(['--product-type', 'landsat'])
         options.extend(['--output-format', 'envi'])
         options.extend(['--target-projection', 'utm'])
 
-        args = self.parser.parse_args(options)
-        self.assertFalse(cli.check_projection_sinu(args))
+        args = parser.parse_args(options)
+        assert not cli.check_projection_sinu(args)
 
+    def test_sinu_specified(self, parser, options):
         # Check missing options
-        options = [x for x in self.static_options]
         options.extend(['--product-type', 'landsat'])
         options.extend(['--output-format', 'envi'])
         options.extend(['--target-projection', 'sinu'])
 
         # Missing central meridian
-        try:
-            args = self.parser.parse_args(options)
+        with pytest.raises(cli.MissingSinuError):
+            args = parser.parse_args(options)
             cli.check_projection_sinu(args)
-        except cli.MissingSinuError:
-            pass
 
         # Missing false easting
         options.extend(['--central-meridian', '-96.0'])
-        try:
-            args = self.parser.parse_args(options)
+        with pytest.raises(cli.MissingSinuError):
+            args = parser.parse_args(options)
             cli.check_projection_sinu(args)
-        except cli.MissingSinuError:
-            pass
 
         # Missing false northing
         options.extend(['--false-easting', '2.0'])
-        try:
-            args = self.parser.parse_args(options)
+        with pytest.raises(cli.MissingSinuError):
+            args = parser.parse_args(options)
             cli.check_projection_sinu(args)
-        except cli.MissingSinuError:
-            pass
 
         # Check all options
         options.extend(['--false-northing', '2.0'])
 
-        args = self.parser.parse_args(options)
-        self.assertTrue(cli.check_projection_sinu(args))
+        args = parser.parse_args(options)
+        assert cli.check_projection_sinu(args)
 
-    def test_aea_specified(self):
-        # Check not aea
-        options = [x for x in self.static_options]
+    def test_not_aea_specified(self, parser, options):
         options.extend(['--product-type', 'landsat'])
         options.extend(['--output-format', 'envi'])
         options.extend(['--target-projection', 'sinu'])
 
-        args = self.parser.parse_args(options)
-        self.assertFalse(cli.check_projection_aea(args))
+        args = parser.parse_args(options)
+        assert not cli.check_projection_aea(args)
+        assert not cli.check_projection_utm(args)
+        assert not cli.check_projection_ps(args)
 
+    def test_aea_specified(self, parser, options):
         # Check missing options
-        options = [x for x in self.static_options]
         options.extend(['--product-type', 'landsat'])
         options.extend(['--output-format', 'envi'])
         options.extend(['--target-projection', 'aea'])
 
         # Missing central meridian
-        try:
-            args = self.parser.parse_args(options)
+        with pytest.raises(cli.MissingAeaError):
+            args = parser.parse_args(options)
             cli.check_projection_aea(args)
-        except cli.MissingAeaError:
-            pass
 
         # Missing std parallel 1
         options.extend(['--central-meridian', '-96.0'])
-        try:
-            args = self.parser.parse_args(options)
+        with pytest.raises(cli.MissingAeaError):
+            args = parser.parse_args(options)
             cli.check_projection_aea(args)
-        except cli.MissingAeaError:
-            pass
 
         # Missing std parallel 2
         options.extend(['--std-parallel-1', '29.0'])
-        try:
-            args = self.parser.parse_args(options)
+        with pytest.raises(cli.MissingAeaError):
+            args = parser.parse_args(options)
             cli.check_projection_aea(args)
-        except cli.MissingAeaError:
-            pass
 
         # Missing origin latitude
         options.extend(['--std-parallel-2', '70.0'])
-        try:
-            args = self.parser.parse_args(options)
+        with pytest.raises(cli.MissingAeaError):
+            args = parser.parse_args(options)
             cli.check_projection_aea(args)
-        except cli.MissingAeaError:
-            pass
 
         # Missing false easting
         options.extend(['--origin-latitude', '40.0'])
-        try:
-            args = self.parser.parse_args(options)
+        with pytest.raises(cli.MissingAeaError):
+            args = parser.parse_args(options)
             cli.check_projection_aea(args)
-        except cli.MissingAeaError:
-            pass
 
         # Missing false northing
         options.extend(['--false-easting', '2.0'])
-        try:
-            args = self.parser.parse_args(options)
+        with pytest.raises(cli.MissingAeaError):
+            args = parser.parse_args(options)
             cli.check_projection_aea(args)
-        except cli.MissingAeaError:
-            pass
 
         # Missing datum
         options.extend(['--false-northing', '2.0'])
-        try:
-            args = self.parser.parse_args(options)
+        with pytest.raises(cli.MissingAeaError):
+            args = parser.parse_args(options)
             cli.check_projection_aea(args)
-        except cli.MissingAeaError:
-            pass
 
         # Check all options
         options.extend(['--datum', 'wgs84'])
 
-        args = self.parser.parse_args(options)
-        self.assertTrue(cli.check_projection_aea(args))
+        args = parser.parse_args(options)
+        assert cli.check_projection_aea(args)
 
-
-    def test_utm_specified(self):
-        # Check not aea
-        options = [x for x in self.static_options]
-        options.extend(['--product-type', 'landsat'])
-        options.extend(['--output-format', 'envi'])
-        options.extend(['--target-projection', 'sinu'])
-
-        args = self.parser.parse_args(options)
-        self.assertFalse(cli.check_projection_utm(args))
-
-        # Check missing options
-        options = [x for x in self.static_options]
+    def test_utm_specified(self, parser, options):
         options.extend(['--product-type', 'landsat'])
         options.extend(['--output-format', 'envi'])
         options.extend(['--target-projection', 'utm'])
 
         # Missing utm zone
-        try:
-            args = self.parser.parse_args(options)
+        with pytest.raises(cli.MissingUtmError):
+            args = parser.parse_args(options)
             cli.check_projection_utm(args)
-        except cli.MissingUtmError:
-            pass
 
         # Missing utm north south
         options.extend(['--utm-zone', '10.0'])
-        try:
-            args = self.parser.parse_args(options)
+        with pytest.raises(cli.MissingUtmError):
+            args = parser.parse_args(options)
             cli.check_projection_utm(args)
-        except cli.MissingUtmError:
-            pass
 
         # Check all options
         options.extend(['--utm-north-south', 'north'])
 
-        args = self.parser.parse_args(options)
-        self.assertTrue(cli.check_projection_utm(args))
+        args = parser.parse_args(options)
+        assert cli.check_projection_utm(args)
 
-    def test_ps_specified(self):
-        # Check not ps
-        options = [x for x in self.static_options]
-        options.extend(['--product-type', 'landsat'])
-        options.extend(['--output-format', 'envi'])
-        options.extend(['--target-projection', 'sinu'])
-
-        args = self.parser.parse_args(options)
-        self.assertFalse(cli.check_projection_ps(args))
-
-        # Check missing options
-        options = [x for x in self.static_options]
+    def test_ps_specified(self, parser, options):
         options.extend(['--product-type', 'landsat'])
         options.extend(['--output-format', 'envi'])
         options.extend(['--target-projection', 'ps'])
 
         # Missing latitude true scale
-        try:
-            args = self.parser.parse_args(options)
+        with pytest.raises(cli.MissingPsError):
+            args = parser.parse_args(options)
             cli.check_projection_ps(args)
-        except cli.MissingPsError:
-            pass
 
         # Missing longitude pole
         options.extend(['--latitude-true-scale', '-90.0'])
-        try:
-            args = self.parser.parse_args(options)
+        with pytest.raises(cli.MissingPsError):
+            args = parser.parse_args(options)
             cli.check_projection_ps(args)
-        except cli.MissingPsError:
-            pass
 
         # Missing origin latitude
         options.extend(['--longitude-pole', '0.0'])
-        try:
-            args = self.parser.parse_args(options)
+        with pytest.raises(cli.MissingPsError):
+            args = parser.parse_args(options)
             cli.check_projection_ps(args)
-        except cli.MissingPsError:
-            pass
 
         # Missing false easting
         options.extend(['--origin-latitude', '-71.0'])
-        try:
-            args = self.parser.parse_args(options)
+        with pytest.raises(cli.MissingPsError):
+            args = parser.parse_args(options)
             cli.check_projection_ps(args)
-        except cli.MissingPsError:
-            pass
 
         # Missing false northing
         options.extend(['--false-easting', '2.0'])
-        try:
-            args = self.parser.parse_args(options)
+        with pytest.raises(cli.MissingPsError):
+            args = parser.parse_args(options)
             cli.check_projection_ps(args)
-        except cli.MissingPsError:
-            pass
 
         # Check all options
         options.extend(['--false-northing', '2.0'])
 
-        args = self.parser.parse_args(options)
-        self.assertTrue(cli.check_projection_ps(args))
+        args = parser.parse_args(options)
+        assert cli.check_projection_ps(args)
