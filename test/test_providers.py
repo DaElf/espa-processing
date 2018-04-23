b20ac0d8c0faec16f9fdb129a4941ec876a14939
Jake Brinkmann <jakebrinkmann@gmail.com>
2018-04-23 12:47:40 -0500

    TST: multi-scene testing

--- test/test_providers.py
+++ test/test_providers.py
@@ -3,19 +3,29 @@ import pytest
 from processing import providers
 
 
-LT04_TEST_ID = 'LT04_L1TP_030031_19890420_20161002_01_T1'
-LT05_TEST_ID = 'LT05_L1TP_032028_20120425_20160830_01_T1'
-LE07_TEST_ID = 'LE07_L1TP_028028_20130510_20160908_01_T1'
-LC08_TEST_ID = 'LC08_L1TP_015035_20140713_20170304_01_T1'
+@pytest.fixture(params=[4,5,7,8])
+def lsat_id(request ):
+    return {
+        4: 'LT04_L1TP_030031_19890420_20161002_01_T1',
+        5: 'LT05_L1TP_032028_20120425_20160830_01_T1',
+        7: 'LE07_L1TP_028028_20130510_20160908_01_T1',
+        8: 'LC08_L1TP_015035_20140713_20170304_01_T1'
+    }[request.param]
 
 
 def test_no_yaml_found():
     assert {} == providers.read_yaml('/path/does/not/exist')
     assert {} == providers.read_yaml('processing', '*.notafile')
 
-def test_ok_yaml_found():
-    assert {} != providers.read_yaml('processing/')
-    assert len(providers.read_yaml('processing/').keys()) > 0
+@pytest.fixture(scope='module')
+def provider_path():
+    import os
+    if os.path.isdir('processing'):
+        return 'processing'
+
+def test_ok_yaml_found(provider_path):
+    assert {} != providers.read_yaml(provider_path)
+    assert len(providers.read_yaml(provider_path).keys()) > 0
 
 def test_make_cmd():
     cmd = 'echo hello'
@@ -40,15 +50,15 @@ def test_fetch_requires_nested():
     expected = [{'cmd': 'buy_car'}, {'cmd': 'go_to_market'}]
     assert expected == providers.fetch_requires(p, ps)
 
-def test_lpgs_to_espa(pid=LT04_TEST_ID):
+def test_lpgs_to_espa(lsat_id, provider_path):
     product = 'espa_landsat'
-    cmd = 'lpgs_to_espa --mtl {}.mtl;'.format(pid)
-    assert providers.sequence(product, pid) == cmd
+    cmd = 'convert_lpgs_to_espa --mtl {}.MTL'.format(lsat_id)
+    assert providers.sequence(product, product_id=lsat_id) == cmd
 
-def test_toa_command(pid=LT04_TEST_ID):
+def test_toa_command(lsat_id, provider_path):
     product = 'toa_refl'
-    cmd = ['surface_reflectance.py --xml {}.xml --write-sr False'.format(pid)]
-    assert providers.sequence(product, pid) == cmd
+    cmd = ['surface_reflectance.py --xml {}.xml --write-sr False'.format(lsat_id)]
+    assert providers.sequence(product, lsat_id) == cmd
 
 def test_bt_command():
     assert 1 < 0
