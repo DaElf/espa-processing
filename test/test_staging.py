e30942af7d17c91bba166fbdad30842e6e93abbf
Jake Brinkmann <jakebrinkmann@gmail.com>
2018-04-23 15:48:45 -0500

    MAINT: rearrange things

--- test/test_staging.py
+++ test/test_staging.py
@@ -1,24 +1,25 @@
 
 import os
 import shutil
+import tempfile
 
 import pytest
 
-from processing import initialization
+from processing import staging
 
 def test_dirs_to_make():
     basedir = '/path/to/working/dir'
     subdirs = ['mynewdir']
-    assert len(subdirs) == len(initialization.dirs_to_make(basedir, subdirs))
-    assert initialization.dirs_to_make(basedir, subdirs)[0].endswith(subdirs[0] )
+    assert len(subdirs) == len(staging.dirs_to_make(basedir, subdirs))
+    assert staging.dirs_to_make(basedir, subdirs)[0].endswith(subdirs[0] )
 
 @pytest.fixture
 def temporary_dir():
-    fullpath = '/path/to/working/dir'
+    fullpath = './make/path/here/'
     yield fullpath
     shutil.rmtree(fullpath, ignore_errors=True)
 
 def test_init_dirs(temporary_dir):
     assert not os.path.exists(temporary_dir)
-    initialization.initialize_processing_directory(temporary_dir)
+    staging.initialize_processing_directory(temporary_dir)
     assert os.path.exists(temporary_dir)
