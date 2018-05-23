4d1f02d53b9dcaf609c72900e1f6c61746024250
Russell Cattelan <cattelan@digitalelves.com>
2018-05-23 23:10:38 +0000

    Q&D build script for now

---
+++ espa-processing/build.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/sh
+set -x
+
+if [ ! -f SOURCES/espa-processing.tar.gz ]; then
+(cd ../../espa-processing; \
+	 git  archive --format=tar.gz \
+	-o ../espa-rpmbuild/espa-processing/SOURCES/espa-processing.tar.gz  \
+	--prefix=espa-processing-1.0/ ceph)
+fi
+rpmbuild --define "_topdir $(pwd)" -bs SPECS/espa-processing.spec
+sudo mock --old-chroot  -r my-epel-7-x86_64 --resultdir $(pwd)/mock_result SRPMS/*.src.rpm
+
