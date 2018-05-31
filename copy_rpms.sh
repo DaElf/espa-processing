ce3984d30eae9a94d7cf238826078ea7151ecfa7
Russell Cattelan <cattelan@digitalelves.com>
2018-05-31 21:20:01 +0000

    Set the root dir for the local repo
    Do not just use $HOME

--- copy_rpms.sh
+++ copy_rpms.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 set -x
 
-find ./ -name \*.x86_64.rpm -exec rsync -aP {}  $HOME/CentOS/7/local/x86_64/RPMS/ \;
-createrepo $HOME/CentOS/7/local/x86_64/
+root=/efs
+
+mkdir -p $root/CentOS/7/local/x86_64/
+find ./ -name \*.x86_64.rpm -exec rsync -aP {}  $root/CentOS/7/local/x86_64/RPMS/ \;
+createrepo $root/CentOS/7/local/x86_64/
