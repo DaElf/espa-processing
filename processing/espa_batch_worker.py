a57ca5fd5ef72440f14c5236f9c96bb3e7285143
John Cavanaugh <jcavanaugh@contractor.usgs.gov>
2018-06-22 20:33:06 +0000

    Initial check-in
    Espa_batch_worker.py is a script that is meant to run in
    an AWS batch environment.  It is started with an order ID as
    the only argument.  It will use the order ID to read an S3
    object containing a JSON definition of a scene processing job
    and will process the job.

---
+++ processing/espa_batch_worker.py
@@ -0,0 +1,70 @@
+#! /usr/bin/env python
+
+
+import os
+import sys
+import shutil
+import socket
+import json
+import boto3
+import logging
+from ConfigParser import ConfigParser
+
+import settings as settings
+import utilities as util
+import config_utils as config
+from logging_tools import EspaLogging
+import processor as processor
+import transfer as transfer
+from cli import archive_log_s3
+from cli import archive_log_files
+from cli import copy_log_file
+from cli import export_environment_variables
+
+from espa_worker import validate_order
+from espa_worker import process_order
+
+
+APP_NAME = 'ESPA-Processing-Worker'
+VERSION = '2.23.0.1'
+
+
+def main():
+    """Read a JSON file with details about an order from
+       an S3 bucket and process the order
+    """
+
+    if len(sys.argv) != 2:
+        sys.stderr.write('Error: expected one argument, got {}'.format(len(sys.argv) - 1))
+        sys.exit(1)
+    order_id = sys.argv[1]
+    s3_key = order_id + '/' + order_id + '.json'
+
+    job_bucket = "jdc-test-dev"  # JDC debug
+    if 'espaJobBucket' in os.environ:
+        job_bucket = os.environ['espaJobBucket']
+    if 'AWSRegion' in os.environ:
+        s3 = boto3.resource('s3', region_name = os.environ['AWSRegion'])
+    else:
+        s3 = boto3.resource('s3')
+
+    s3 = boto3.resource("s3").Bucket(job_bucket)
+    try:
+        json.load_s3 = lambda f: json.load(s3.Object(key=f).get()["Body"])
+        order = json.load_s3(s3_key)
+    except Exception as e:
+        sys.stderr.write("Can't load JSON object from s3: {}".format(e))
+        sys.exit(1)
+
+    if not validate_order(order):
+        sys.stderr.write('Invalid order')
+        sys.exit(1)
+
+    # JDC Debug
+    print("Processing order " + order['orderid'])
+    print json.dumps(order, sort_keys=True, indent=4, separators=(',', ': '))
+    process_order(order)
+
+
+if __name__ == '__main__':
+    main()
