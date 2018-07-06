c7721975c886b806112820a05a59052c9eebddbb
Russell Cattelan <cattelan@digitalelves.com>
2018-07-06 19:54:16 +0000

    Update to use user defined rpm repo

--- build_me.sh
+++ build_me.sh
@@ -2,7 +2,9 @@
 set -x
 set -e
 
-pushd /efs;
+root=/devel/$USER
+
+pushd $root;
 python -m SimpleHTTPServer 9000 &
 pid=$! ;  echo "simple server pid $pid"
 popd
@@ -24,7 +26,7 @@ for repo in $1; do
 	      -o ../espa-rpmbuild/$repo/SOURCES/$repo.tar.gz  \
 	      --prefix=$repo/ master)
     (cd $repo; \
-     rm -f SRPMS/*.src.rpm; rm -rf mock_result;  \
+     rm -f SRPMS/*.src.rpm; sudo rm -rf mock_result;  \
      rpmbuild --define "_topdir $(pwd)" --define "dist $my_dist" -bs SPECS/$repo.spec \
 	 && sudo mock $old_chroot --configdir=$(pwd)/../mock_config -r my-epel-7-x86_64 --define "dist $my_dist" \
 		 --resultdir $(pwd)/mock_result SRPMS/*${my_dist}*.src.rpm \
@@ -35,23 +37,19 @@ for repo in $1; do
 done
 }
 
-REPOS_no="\
+REPOS="\
 	geotiff \
 	hdf4 \
 	HDF-EOS \
 	gdal"
 
-REPOS="\
-	geotiff"
-
 # for building inside a docker container
 #old_chroot="--no-clean --old-chroot" 
 old_chroot=""
-root=/efs
 
-pushd ../../ips-all/ips_rpmbuild/
+#pushd ../../ips-all/ips_rpmbuild/
 #process_repos "$REPOS"
-popd
+#popd
 
 REPOS="\
      espa-product-formatter \
