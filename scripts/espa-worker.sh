e84295c0d029ce027dfc6f7b131c2fe2c368a34e
Russell Cattelan <cattelan@digitalelves.com>
2018-07-18 18:15:32 +0000

    pull processing.conf

--- scripts/espa-worker.sh
+++ scripts/espa-worker.sh
@@ -8,6 +8,8 @@ sudo -E -u espa -s <<EOF
 set -x
 env
 
+aws s3 cp s3://a.out/espa-processing.conf $my_dir/processing.conf
+export ESPA_CONFIG_PATH=$my_dir
 gpg --import ./gpg-tools/USGS_private.gpg || true
 (cd $my_dir; time espa-batch-worker $1)
 
